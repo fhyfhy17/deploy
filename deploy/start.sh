cd82661a83300c9e2ba33550e11b18953d1fcf42
fhyfhy17 <fhyfhy17@163.com>
2018-05-02 16:57:44 +0800

    sadf

--- deploy/start.sh
+++ deploy/start.sh
@@ -1,1 +1,17 @@
-nohup java -jar deploy/lib/deploy.jar &
+#!/bin/sh
+
+MAIN_CLASS=com.Main
+RESOURCES_DIR=$DEPLOY_DIR/resources
+
+LIB_JARS=`ls $LIB_DIR|grep .jar|awk '{print "'$LIB_DIR'/"$0}'|tr "\n" ":"`
+for xml in $RESOURCES_DIR/*.xml
+	do LIB_JARS=$LIB_JARS:$xml
+done
+for env in $RESOURCES_DIR/*.properties
+	do LIB_JARS=$LIB_JARS:$env
+done
+
+nohup java -jar $LIB_JARS $MAIN_CLASS &
+
+
+#nohup java -jar deploy/lib/deploy.jar &
