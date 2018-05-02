5e5f168ffe2a596c2af46262e71f4938ff650a68
fhyfhy17 <fhyfhy17@163.com>
2018-05-02 15:25:01 +0800

    update Dockerfile

--- deploy/src/main/java/com/Main.java
+++ deploy/src/main/java/com/Main.java
@@ -1,10 +1,14 @@
 package com;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class Main {
+    static Logger logger = LoggerFactory.getLogger(Main.class);
 	public static void main(String[] args) {
 	    while(true) {
 	        try {
-	            System.out.println("日了狗了");
+	            logger.info("日了狗了");
                 Thread.sleep(2000);
             } catch (InterruptedException e) {
                 e.printStackTrace();
