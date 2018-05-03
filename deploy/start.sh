cdd61569aec84992db793beebb3d0cfe3da85ec5
fhyfhy17 <fhyfhy17@163.com>
2018-05-03 10:33:39 +0800

    sadf

--- deploy/start.sh
+++ deploy/start.sh
@@ -1,17 +1,11 @@
 #!/bin/sh
+DEPLOY_DIR=`pwd`
+echo $DEPLOY_DIR
+STDOUT=`pwd`/stdout.log
+STDERR=`pwd`/stderr.log
+APPID=got
+CLASS_PATH=./:./lib/*
 
 MAIN_CLASS=com.Main
-RESOURCES_DIR=$DEPLOY_DIR/resources
-
-LIB_JARS=`ls $LIB_DIR|grep .jar|awk '{print "'$LIB_DIR'/"$0}'|tr "\n" ":"`
-for xml in $RESOURCES_DIR/*.xml
-	do LIB_JARS=$LIB_JARS:$xml
-done
-for env in $RESOURCES_DIR/*.properties
-	do LIB_JARS=$LIB_JARS:$env
-done
-
-nohup java -jar $LIB_JARS $MAIN_CLASS &
-
-
-#nohup java -jar deploy/lib/deploy.jar &
+RESOURCES_DIR=$DEPLOY_DIR/resources          
+java -D$APPID -Xms128m -Xmx128m -server -XX:+UseG1GC -XX:+UseNUMA -XX:+PrintGCDetails -Xloggc:gc.log -cp $CLASS_PATH $MAIN_CLASS>>$STDOUT 2>>$STDERR&
