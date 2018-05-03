941a90d48e95815961aecce49cfed2acbe3e4f8a
fhyfhy17 <fhyfhy17@163.com>
2018-05-03 12:51:59 +0800

    sadf

--- deploy/start.sh
+++ deploy/start.sh
@@ -6,7 +6,7 @@ STDERR=$pwd/logs/stderr.log
 test -d $pwd/logs/ || mkdir -p $pwd/logs
 APPID=got
 CLASS_PATH=$pwd/:./lib/*
-echo CLASS_PATH
+echo $CLASS_PATH
 MAIN_CLASS=com.Main
 RESOURCES_DIR=$pwd/resources          
 /usr/bin/java -D$APPID -Xms128m -Xmx128m -server -XX:+UseG1GC -XX:+UseNUMA -XX:+PrintGCDetails -Xloggc:gc.log \
