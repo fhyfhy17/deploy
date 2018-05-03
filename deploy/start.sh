c73b3d8ae4eccae520b911763be972ee7e027eab
fhyfhy17 <fhyfhy17@163.com>
2018-05-03 12:06:23 +0800

    sadf

--- deploy/start.sh
+++ deploy/start.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
-DEPLOY_DIR=`pwd`
-echo $DEPLOY_DIR
-STDOUT=`pwd`/logs/stdout.log
-STDERR=`pwd`/logs/stderr.log
-test -d `pwd`/logs/ || mkdir -p `pwd`/logs
+pwd=$(cd "$(dirname "$0")"; pwd)
+echo $pwd
+STDOUT=$pwd/logs/stdout.log
+STDERR=$pwd/logs/stderr.log
+test -d $pwd/logs/ || mkdir -p $pwd/logs
 APPID=got
 CLASS_PATH=./:./lib/*
 
 MAIN_CLASS=com.Main
-RESOURCES_DIR=$DEPLOY_DIR/resources          
+RESOURCES_DIR=$pwd/resources          
 /usr/bin/java -D$APPID -Xms128m -Xmx128m -server -XX:+UseG1GC -XX:+UseNUMA -XX:+PrintGCDetails -Xloggc:gc.log \
 -cp $CLASS_PATH $MAIN_CLASS>>$STDOUT 2>>$STDERR&
