bb85e083d2e7dba3896d5f238901dba86c1f188e
fhyfhy17 <fhyfhy17@163.com>
2018-05-03 12:54:16 +0800

    sadf

--- deploy/start.sh
+++ deploy/start.sh
@@ -5,7 +5,7 @@ STDOUT=$pwd/logs/stdout.log
 STDERR=$pwd/logs/stderr.log
 test -d $pwd/logs/ || mkdir -p $pwd/logs
 APPID=got
-CLASS_PATH=$pwd/:./lib/*
+CLASS_PATH=$pwd/lib/*
 echo $CLASS_PATH
 MAIN_CLASS=com.Main
 RESOURCES_DIR=$pwd/resources          
